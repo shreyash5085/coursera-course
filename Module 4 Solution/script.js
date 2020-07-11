e1c27eab65f14ee3a7eb7f3c5a5ca6614b2b0662
shreyash5085 <66836845+shreyash5085@users.noreply.github.com>
2020-07-11 23:10:33 +0530

    Add files via upload

---
+++ Module 4 Solution/script.js
@@ -0,0 +1,28 @@
+// ******************************* 
+// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// , otherwise, the program should say "Good Bye". So, the final output*/
+
+
+(function () {
+
+var names = ["Tom", "John", "Jerry", "Jason", "Jessica", "Emma", "Ross", "Joe", "Mark", "Julia"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
